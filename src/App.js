fdaf2467c78b2dab9ecacdf39f4a6a9b1b431edd
NameerWaqas <nameerwaqas321@gmail.com>
2020-08-03 15:17:51 +0500

    Initialized an empty React Project for Algorithms simulator web app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <h1>Algorithm Simulator.</h1>
+        {/* <button>Nameer</button> */}
+    </div>
+  )
+}
+
+export default App;
